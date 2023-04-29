9932817859a8d3a6156bdea2ccb2ba60ace2a243
d1zzy <d1zzzy.project@gmail.com>
2023-04-29 19:23:32 +0300

    ready project 3

--- How-long-to-go-to-school-1e6b434d0ba7218a3248736ee1b918e9ea77012b/getting.py
+++
@@ -1,7 +0,0 @@
-def getting(response):
-    json_response = response.json()
-    toponym = json_response["response"]["GeoObjectCollection"][
-        "featureMember"][0]["GeoObject"]
-    toponym_coodrinates = toponym["Point"]["pos"]
-    toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
-    return (float(toponym_longitude), float(toponym_lattitude))
